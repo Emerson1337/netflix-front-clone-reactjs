be25747fc6fd3520233b8fee7d0c964d910b4b46
Emerson <emersonlucenaoficial@gmail.com>
2021-08-17 20:40:20 -0300

    first commit with some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+//COMPONENTS
+import MovieRow from './components/MovieRow';
+
+//API
+import Tmdb from './Tmdb';
+
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //PEGANDO LISTA DOS FILMES
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <section className="lists">
+        <div>
+          {movieList.map((item, key) => (
+            <MovieRow key={key} title={item.title} items={item.items} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
