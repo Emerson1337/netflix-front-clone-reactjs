5217d8ffdaa5236afbb2670a06d559bac2374cbd
Emerson <emersonlucenaoficial@gmail.com>
2021-08-18 12:24:43 -0300

    added some effects on hover, new styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 //COMPONENTS
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 //API
 import Tmdb from './Tmdb';
@@ -10,12 +11,22 @@ import Tmdb from './Tmdb';
 function App() {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       //PEGANDO LISTA DOS FILMES
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //PEGANDO O FILME EM DESTAQUE
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData(chosenInfo);
+
     }
 
     loadAll();
@@ -23,6 +34,10 @@ function App() {
 
   return (
     <div className="page">
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
         <div>
           {movieList.map((item, key) => (
